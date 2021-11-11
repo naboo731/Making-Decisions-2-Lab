4ddf981aa044853e021696d9d1cda7ab35053779
Nichole Boothman <nbooth731@gmail.com>
2021-11-11 18:01:06 -0500

    Problem 9 solved

--- making decisions 2.js
+++ making decisions 2.js
@@ -121,7 +121,7 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 
 for (let i = 0; i < listOfNumbers.length; i++){
   if (listOfNumbers[i] % 3 === 0){
-    console.log(listOfNumbers[i] + " is divisible by 3")
+    // console.log(listOfNumbers[i] + " is divisible by 3")
   }
 }
 
@@ -134,6 +134,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 /*
   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
 */
+
+for (let i = letters.length - 1; i >= 0; i--){
+  console.log(letters[i])
+}
+
 
 
 ////////// Advanced Problems //////////
